baf147e8d0b01a7a1a2c5f018e879038908055f2
Affan Ahmed Khan <affan2k3@gmail.com>
2024-03-23 02:40:24 +0500

    callBack

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 console.log("Before")
+const data = getUser(1, (user) => {
+
+
+    // 
+    getRepos(user.username, (repos) => {
+        console.log("Repos:",repos)
+    }) 
+})
 console.log("After")
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log("Reading user from database")
+        callback({id:id, username: "affan"})
+    }, 2000);
+}
+
+function getRepos(id,callback) {
+    setTimeout(() => {
+        console.log("Getting Repos")
+        callback([
+            'repo1','repo2','repo3','repo4'
+        ])
+    }, 2000);
+}
